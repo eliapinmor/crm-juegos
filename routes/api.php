a6ac69cea19a62c0a02636f0ea50825739d997a0
elia <eliapinmor@monlau.com>
2026-04-21 11:50:21 +0200

    distribucion de rutas api y web

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::middleware('auth:sanctum')->group(function () {
-    // Route::post('/game-sessions/start', [GameSessionController::class, 'start']);
-    // Route::post('/game-sessions/{id}/end', [GameSessionController::class, 'end']);
-    // Route::post('/emotions', [EmotionController::class, 'store']);
-// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/game-sessions/start', [GameSessionController::class, 'start']);
+    Route::post('/game-sessions/{id}/end', [GameSessionController::class, 'end']);
+    Route::post('/emotions', [EmotionController::class, 'store']);
+});
